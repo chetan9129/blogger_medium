a3e960c89f6d6bdde7b11aae1439fbe5180737e6
chetan9129 <anku9129@gmail.com>
2023-06-06 00:39:24 +0530

    Backend Changes

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,18 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
+const dotenv = require("dotenv");
+
 const cors = require("cors");
 const path = require("path");
 const app = express();
 const router = require("./routers");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8080;
+dotenv.config({ path: "./config.env" });
 app.use(cors());
+const PORT = process.env.PORT || 8080;
 
 const db = require("./db");
-db.connect();
 
 app.use(bodyParser.json({ limit: "500mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "500mb" }));
@@ -23,8 +25,10 @@ app.use((req, res, next) => {
   res.header("Access-Control-Request-Method", "*");
   next();
 });
+
 const usersRoute = require("./routers/User");
 const storiesRoute = require("./routers/Stories");
+
 console.log(process.env.MONGO_URL);
 
 app.use("/api/user", usersRoute);
